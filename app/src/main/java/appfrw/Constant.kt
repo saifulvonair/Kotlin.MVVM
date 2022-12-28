b644f46b9863b0a9a91bf63de18cba042efa9529
Saiful <studybuddybd18@gmail.com>
2022-12-28 10:43:36 +0600

    Added screen size to main image

--- app/src/main/java/appfrw/Constant.kt
+++ app/src/main/java/appfrw/Constant.kt
@@ -1,19 +1,28 @@
 package appfrw
 
+import android.content.res.Resources
+
 class Constant {
 
     //   location=23.810331,90.412521&radius=1500&type=restaurant&key=AIzaSyBLzNu8HIdVsLYVEnHS0cNgShjSCVXlOOA"
     companion object {
+
+        /*
+        var width: Int = Resources.getSystem().displayMetrics.widthPixels
+        var height: Int = Resources.getSystem().displayMetrics.heightPixels
+        var wd = width/2
+        var ht = wd * 0.67
+       */
         @JvmStatic
         val mWebUrlRoot: String = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
         @JvmStatic
-        val mApiKey: String = "...."
+        val mApiKey: String = "AIzaSyBLzNu8HIdVsLYVEnHS0cNgShjSCVXlOOA"
         @JvmStatic
         val mGooglePhotoUrlRoot: String = "https://maps.googleapis.com/maps/api/place/photo"
         @JvmStatic
-        val  mThumbnailWidth = 200
+        val  mThumbnailWidth = Resources.getSystem().displayMetrics.widthPixels/2
         @JvmStatic
-        val  mMaxPhotoWidth = 500
+        val  mMaxPhotoWidth = Resources.getSystem().displayMetrics.widthPixels
         @JvmStatic
         var  mRadius = 5000
         @JvmStatic
